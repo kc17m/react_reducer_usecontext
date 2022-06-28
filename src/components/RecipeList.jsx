89b6b10872c74f5f97ab9b82471c36ccf26d3e41
kc17m <64145266+kc17m@users.noreply.github.com>
2022-06-28 19:09:56 +0200

    dark mode

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
 //styles
 import "./RecipeList.css";
 
 const RecipeList = ({ recipes }) => {
+  const { mode } = useTheme();
+
   if (recipes.length === 0) {
     return <div>No recipies found</div>; // if returned data arr is empty, it returns different template if recipies is empty
   }
@@ -11,7 +14,7 @@ const RecipeList = ({ recipes }) => {
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="card">
+        <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make</p>
           <div>{recipe.method.slice(0, 100)}...</div>
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -6,6 +6,8 @@ const themeReducer = (state, action) => {
   switch (action.type) {
     case "CHANGE_COLOR":
       return { ...state, color: action.payload }; //spread syntax to keep all "state" properties if more than one after the state change
+    case "CHANGE_MODE":
+      return { ...state, mode: action.payload };
     default:
       return state;
   }
@@ -19,14 +21,19 @@ export function ThemeProvider({ children }) {
     //analog zu useState: state und dispatch function
     //dispatch: takes action object as argument: action.type, action.payload
     color: "#58249c",
+    mode: "dark",
   });
 
   const changeColor = (color) => {
     dispatch({ type: "CHANGE_COLOR", payload: color }); //action.type + action.payload
   };
 
+  const changeMode = (mode) => {
+    dispatch({ type: "CHANGE_MODE", payload: mode });
+  };
+
   return (
-    <ThemeContext.Provider value={{ ...state, changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
     </ThemeContext.Provider>
   );
